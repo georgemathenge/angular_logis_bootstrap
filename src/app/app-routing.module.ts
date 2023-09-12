6e229fb9a453dd37e9390a847d99f3f7f1c951db
George <georgemathenge957@gmail.com>
2023-09-12 15:36:29 +0300

    estates_migrate_to angular 001

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './views/welcome-page/welcome-page.component';
+import { GalleryComponent } from './views/gallery/gallery.component';
+import { AboutComponent } from './views/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { WelcomePageComponent } from './views/welcome-page/welcome-page.component';
+import { GalleryComponent } from './views/gallery/gallery.component';
+import { AboutComponent } from './views/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    WelcomePageComponent,
+    GalleryComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
